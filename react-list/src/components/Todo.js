26849edcee8b88f48c1cc6cbfe1df10aa240b303
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-20 01:17:34 +0530

    Added CreateList Functionality

---
+++ react-list/src/components/Todo.js
@@ -0,0 +1,23 @@
+import React,{useState} from 'react'
+import TodoList from './TodoList'
+import { RiCloseCircleLine} from 'react-icons/ri'
+import { TiEdit } from 'react-icons/ti'
+function Todo({todos, completeTodo}) {
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''
+    }); 
+    return todos.map((todo, index) => {
+        <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+
+            </div>
+            <div className="icons">
+                <RiCloseCircleLine />
+                <TiEdit />
+            </div>
+        </div>
+    })
+}
+ 
+export default Todo
