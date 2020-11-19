6d94d55a3ac80657d0f260ed275770773dfe98f0
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-19 23:57:19 +0530

    Added ListForm

--- react-list/src/App.js
+++ react-list/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>To Do List</h1>
+      <TodoList />
     </div>
   );
 }
---
+++ react-list/src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+import TodoForm from './TodoForm'
+
+function TodoList() {
+    return (
+        <div>
+            <h1>Yahi patak ke chod denge</h1>
+            <TodoForm />
+        </div>
+    )
+}
+
+export default TodoList
