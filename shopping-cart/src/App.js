f7e0a02a6b6c65ecc3785aa274594d29be52936c
Kumar Nikhil <knikhil352000@gmail.com>
2021-01-14 16:13:56 +0530

    Setting Up the actions

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -6,12 +6,40 @@ import CartContainer from "./components/CartContainer";
 import {cartItems} from "./cart-items";
 // redux stuff
 
+import {createStore} from 'redux';
+import { DECREASE, INCREASE } from "./action";
+// reducer
+// initial state
+
+const initialState = {
+  count: 0
+};
+
+const reducer = (state = initialState, action) => {
+  if(action.type === DECREASE) {
+    return {
+      ...state,
+      count: state.count - 1
+    }
+  }
+  if(action.type === INCREASE) {
+    return {
+      ...state,
+      count: state.count + 1
+    }
+  }
+  return state;
+}
+const store = createStore(reducer, initialState);
+
+store.dispatch({type: INCREASE})
+
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()}/>
       <CartContainer cart={cartItems} />
     </main>
   );
