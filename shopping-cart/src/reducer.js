e57c8e257aa99044ca8fe943377936caf73cd2d7
Kumar Nikhil <knikhil352000@gmail.com>
2021-01-14 22:00:29 +0530

    Increase and decrease functionality

--- shopping-cart/src/reducer.js
+++ shopping-cart/src/reducer.js
@@ -2,15 +2,32 @@ import { CLEAR_CART, DECREASE, INCREASE, REMOVE } from "./action";
 import { cartItems } from "./cart-items";
 const reducer = (state, action) => {
     if(action.type === DECREASE) {
+        let tempCart = [];
+        if(action.payload.amount === 1) {
+            tempCart = state.cart.filter((cartItem) => cartItem.id !== action.payload.id);
+        } else {
+            tempCart = state.cart.map(cartItem => {
+                if(cartItem.id === action.payload.id) {
+                    cartItem = {...cartItem, amount: cartItem.amount - 1}
+                }
+                return cartItem;
+            })
+        }
       return {
         ...state,
-        count: state.count - 1
+        cart: tempCart
       }
     }
     if(action.type === INCREASE) {
+        let tempCart = state.cart.map(cartItem => {
+            if(cartItem.id === action.payload.id) {
+                cartItem = {...cartItem, amount: cartItem.amount + 1}
+            }
+            return cartItem;
+        })
       return {
         ...state,
-        count: state.count + 1
+        cart: tempCart
       }
     }
     if(action.type === CLEAR_CART){
