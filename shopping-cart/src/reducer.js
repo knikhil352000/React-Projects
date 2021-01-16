0e99aef57e32347b89ff51d045d92d1b4741504e
Kumar Nikhil <knikhil352000@gmail.com>
2021-01-17 02:06:55 +0530

    Completed Shopping Cart

--- shopping-cart/src/reducer.js
+++ shopping-cart/src/reducer.js
@@ -1,5 +1,4 @@
 import { CLEAR_CART, DECREASE, GET_TOTALS, INCREASE, REMOVE } from "./action";
-import { cartItems } from "./cart-items";
 const reducer = (state, action) => {
     if(action.type === DECREASE) {
         let tempCart = [];
@@ -45,14 +44,19 @@ const reducer = (state, action) => {
     if(action.type === GET_TOTALS) {
         let {total, amount} = state.cart.reduce((cartTotal, cartItem) => {
             const { price, amount } = cartItem;
-            return cartTotal;//Not able to Understand
+            const itemTotal = price * amount;
+
+            cartTotal.total += itemTotal;
+            cartTotal.amount += amount;
+            return cartTotal;
         },
         {
             total: 0,
             amount: 0
         })
+        total = parseFloat(total.toFixed(2));
         return {
-            ...state
+            ...state, total, amount
         }
     }
     return state;
