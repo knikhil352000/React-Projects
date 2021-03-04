04cc3cc05c0a206a41769dc4c4d91b450a47af7a
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-04 23:23:18 +0530

    Creating chat front screen

---
+++ Tinder-clone/src/Chats.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Chats.css'
+const Chats = () => {
+    return (
+        <div className='chats'>
+            <Chat 
+                name='Sandra'
+                message='Hii Whats up !'
+                timestamp='40 seconds ago'
+                profilePic='https://i.insider.com/5c350e4c01c0ea12db0b25f5?width=1000&format=jpeg&auto=webp'
+            />
+            <Chat 
+                name='Sandra'
+                message='Hii Whats up !'
+                timestamp='40 seconds ago'
+                profilePic='https://i.insider.com/5c350e4c01c0ea12db0b25f5?width=1000&format=jpeg&auto=webp'
+            />
+            <Chat 
+                name='Sandra'
+                message='Hii Whats up !'
+                timestamp='40 seconds ago'
+                profilePic='https://i.insider.com/5c350e4c01c0ea12db0b25f5?width=1000&format=jpeg&auto=webp'
+            />
+            <Chat 
+                name='Sandra'
+                message='Hii Whats up !'
+                timestamp='40 seconds ago'
+                profilePic='https://i.insider.com/5c350e4c01c0ea12db0b25f5?width=1000&format=jpeg&auto=webp'
+            />
+
+        </div>
+    )
+}
+
+export default Chats
