364bb27b1e9a6dd5fcd9ee31887a47ce2d031548
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-05 01:18:24 +0530

    Finishing up tinder clone

--- Tinder-clone/src/Chat.js
+++ Tinder-clone/src/Chat.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import Avatar from '@material-ui/core/Avatar'
 import './Chats.css'
+import { Link } from 'react-router-dom'
 const Chat = ({ name, message, profilePic, timestamp}) => {
     return (
-        <div className='chat'>
-            <Avatar className='chat__image' alt={name} src={profilePic}/>
-            <div className='chat__details'>
-                <h2>{name}</h2>
-                <p>{message}</p>
+        <Link to={`/chat/${name}`}>
+            <div className='chat'>
+                <Avatar className='chat__image' alt={name} src={profilePic}/>
+                <div className='chat__details'>
+                    <h2>{name}</h2>
+                    <p>{message}</p>
+                </div>
+                <p className='chat__timestamp'>{timestamp}</p>
             </div>
-            <p className='chat__timestamp'>{timestamp}</p>
-        </div>
+        </Link>
+        
     )
 }
 
