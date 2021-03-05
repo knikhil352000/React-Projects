e29e17b7cbdb682e4ac03c2ac271bf0b57eaa34e
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-06 01:02:56 +0530

    Completing the header

--- Youtube-clone/src/Header.js
+++ Youtube-clone/src/Header.js
@@ -9,14 +9,20 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <MenuIcon />
-            <img className='header__logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/753px-Logo_of_YouTube_%282015-2017%29.svg.png" alt=""/>
-            <input type="text"/>
-            <SearchIcon />
-            <VideoCallIcon/>
-            <AppsIcon />
-            <NotificationsIcon />
-            <Avatar alt='Nikhil' src='https://avatars.githubusercontent.com/u/71940958?s=460&u=24ef1c10f91e5ef2dffac627465b825a9fef769a&v=4'/>
+            <div className="header__left">
+                <MenuIcon />
+                <img className='header__logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/753px-Logo_of_YouTube_%282015-2017%29.svg.png" alt=""/>
+            </div>
+            <div className="header__input">
+                <input type="text" placeholder='Search'/>
+                <SearchIcon className='header__inputButton'/>
+            </div>
+            <div className="header__icons">
+                <VideoCallIcon className='header__icon' />
+                <AppsIcon className='header__icon' />
+                <NotificationsIcon className='header__icon' />
+                <Avatar className='header__icon' alt='Nikhil' src='https://avatars.githubusercontent.com/u/71940958?s=460&u=24ef1c10f91e5ef2dffac627465b825a9fef769a&v=4'/>
+            </div>
         </div>
     )
 }
