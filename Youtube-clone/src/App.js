199d4c9eefdd49893b7bb0fc1ba91edb394225c2
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-05 23:58:28 +0530

    Youtube initial commit

---
+++ Youtube-clone/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import SwipeButtons from './SwipeButtons'
+import TinderCards from './TinderCards'
+
+const App = () => {
+    return (
+        <div className="app">
+        </div>
+    )
+    
+}
+
+
+export default App;
