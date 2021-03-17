dd29b1fdb37b70385fb4c7734dd238326796c3a6
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-17 15:54:35 +0530

    Fetching cases data: API call

--- Covid-Tracker/src/App.js
+++ Covid-Tracker/src/App.js
@@ -4,15 +4,28 @@ import './App.css'
 import InfoBox from './InfoBox'
 import Map from './Map'
 const App = () => {
-    const [countries, setCountries] = useState([
-        'USA', 'UK', 'INDIA', 'BANGLADESH'
-    ])
+    const [countries, setCountries] = useState([])
     const [country, setCountry] = useState('worldwide');
-
+    const [countryInfo, setCountryInfo] = useState({});
     const onCounrtyChange = async(e) => {
         const countryCode = e.target.value;
-        setCountry(countryCode);
+        const url = countryCode === 'worldwide' 
+            ? 'https://disease.sh/v3/covid-19/all' 
+            : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+            await fetch(url) 
+            .then(response => response.json())
+            .then(data => {
+                setCountryInfo(data);
+                setCountry(countryCode);
+            })
     }
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/all')
+        .then(response => response.json())
+        .then(data => {
+            setCountryInfo(data);
+        })
+    }, [])
 
     useEffect(() => {
         const getCountriesData = async() => {
@@ -52,9 +65,9 @@ const App = () => {
                     </FormControl>
                 </div>
                 <div className="app__stats">
-                    <InfoBox title='Coronavirus Cases' cases='123' total={2000}/>
-                    <InfoBox title='Recovered' cases='123' total={3000}/>
-                    <InfoBox title='Deaths' cases='123' total={5000}/>
+                    <InfoBox title='Coronavirus Cases' cases={countryInfo.todayCases} total={countryInfo.cases}/>
+                    <InfoBox title='Recovered' cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+                    <InfoBox title='Deaths' cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
                 </div>
                 <Map />
             </div>
@@ -64,8 +77,6 @@ const App = () => {
                     <h3>Worldwide new cases</h3>
                 </CardContent>
             </Card>
-                {/* Table */}
-                {/* Map */}
         </div>
     )
 }
