f9e4224b4a6035a4939daa2e0c93adbd9b0a2b4a
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-18 00:40:27 +0530

    Styling the map

--- Covid-Tracker/src/utils.js
+++ Covid-Tracker/src/utils.js
@@ -45,13 +45,13 @@ export const showDataOnMap = (data, caseType='cases') => (
                 Math.sqrt(country[caseType]) * caseTypeColors[caseType].multiplier
             }
         >
-            <Popup>
-                <div>
-                    <div>{country.country}</div>
-                    <div style={{backgroundImage: `url(${country.countryInfo.flag})`}}></div>
-                    <div>Cases: {numeral(country.cases).format('0.0')}</div>
-                    <div>Recovered: {numeral(country.recovered).format('0.0')}</div>
-                    <div>Deaths: {numeral(country.deaths).format('0.0')}</div>
+            <Popup >
+                <div className='info-container'>
+                    <div className='info-flag' style={{backgroundImage: `url(${country.countryInfo.flag})`}}></div>
+                    <div className='info-name'>{country.country}</div>
+                    <div className='info-confirmed'>Cases: {numeral(country.cases).format('0.0')}</div>
+                    <div className='info-recovered'>Recovered: {numeral(country.recovered).format('0.0')}</div>
+                    <div className='info-deaths'>Deaths: {numeral(country.deaths).format('0.0')}</div>
                 </div>
             </Popup>
         </Circle>
