c69129f6ddfd534abb34dac8777d4251c294905a
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-17 14:28:27 +0530

    Setting up the header

--- Covid-Tracker/src/App.js
+++ Covid-Tracker/src/App.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { FormControl, MenuItem, Select } from '@material-ui/core'
+import './App.css'
 const App = () => {
     return (
-        <h1>Hello World</h1>
+        <div className="app">
+            <div className="app__header">
+                <h1>COVID 19 TRACKER</h1>
+                <FormControl className='app__dropdown'>
+                    <Select 
+                        variant='outlined'
+                        value='abc'
+                    >
+                        <MenuItem value='worldwide'>Worldwide</MenuItem>
+                        <MenuItem value='worldwide'>Worldwide</MenuItem>
+                        <MenuItem value='worldwide'>Worldwide</MenuItem>
+                        <MenuItem value='worldwide'>Worldwide</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            
+            {/* Header */}
+            {/* Titel + Input dropdown */}
+            {/* InfoBox */}
+            {/* InfoBox */}
+            {/* InfoBox */}
+            {/* Table */}
+            {/* Map */}
+        </div>
     )
 }
 
