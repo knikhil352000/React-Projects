b35d384fa0eda844ebe050224acab55813a964b8
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-23 23:57:35 +0530

    Imessage Initial Commit

---
+++ IMessage-clone/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import SwipeButtons from './SwipeButtons'
+import TinderCards from './TinderCards'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Chats from './Chats'
+import ChatScreen from './ChatScreen'
+const App = () => {
+    return (
+        <div className="app">
+
+        </div>
+    )
+    
+}
+
+
+export default App;
