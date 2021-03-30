09a7d86033cc9212904e4ac841cc0ae615eb23de
knikhil352000 <knikhil352000@gmail.com>
2021-03-30 22:29:41 +0530

    Completing chat header and sending input field

--- IMessage-clone/src/Chat.js
+++ IMessage-clone/src/Chat.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Chat.css'
 const Chat = () => {
+    const [input, setInput] = useState('')
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        setInput('');
+    }
     return (
         <div className='chat'>
-            
+            <div className="chat__header">
+                <h4>To: <span className='chat__name'>Channel name</span></h4>
+                <strong>Details</strong>
+            </div>
+            {/* Chat messages */}
+            <div className="chat__input">
+                <form>
+                    <input
+                        value={input} 
+                        onChange={e => setInput(e.target.value)} 
+                        placeholder='iMessage' 
+                        type="text"
+                    />
+                    <button onClick={sendMessage}>Send Message</button>
+                </form>
+            </div>
         </div>
     )
 }
