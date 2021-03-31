ca8ff594ad828ebc7870e933dbba8e0224994df5
knikhil352000 <knikhil352000@gmail.com>
2021-03-31 21:09:26 +0530

    Setting up firebase

---
+++ IMessage-clone/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyAfhBZ77D7_O7BDiG21LsZtbSTw1S2nfaI",
+    authDomain: "imessage-clone-36eba.firebaseapp.com",
+    projectId: "imessage-clone-36eba",
+    storageBucket: "imessage-clone-36eba.appspot.com",
+    messagingSenderId: "73992590794",
+    appId: "1:73992590794:web:c8cd7f4c01492cc65a2b33"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
